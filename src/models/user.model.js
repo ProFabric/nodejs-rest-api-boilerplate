c321b4cf4f7e9d970619cc0078217de1679a1fd2
Erdi Kose <me@erdkse.com>
2020-02-04 22:45:55 +0300

    remove useTabs from prettirrc

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,74 +9,74 @@ const { passwordRegex } = require('./../validations/user.validation');
 const constants = require('./../../config/constants');
 
 const UserSchema = new mongoose.Schema(
-	{
-		email: {
-			type: String,
-			unique: true,
-			required: [true, 'Email is required'],
-			trim: true,
-			validate: {
-				validator(email) {
-					return validator.isEmail(email);
-				},
-				message: '{VALUE} is not valid email!'
-			}
-		},
-		password: {
-			type: String,
-			required: [true, 'Password is required'],
-			trim: true,
-			minlength: [6, 'Password needs to be longer!'],
-			validate: {
-				validator(password) {
-					return passwordRegex.test(password);
-				},
-				message: '{VALUE} is not valid password'
-			}
-		}
-	},
-	{ timestamps: true }
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'Email is required'],
+      trim: true,
+      validate: {
+        validator(email) {
+          return validator.isEmail(email);
+        },
+        message: '{VALUE} is not valid email!'
+      }
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      trim: true,
+      minlength: [6, 'Password needs to be longer!'],
+      validate: {
+        validator(password) {
+          return passwordRegex.test(password);
+        },
+        message: '{VALUE} is not valid password'
+      }
+    }
+  },
+  { timestamps: true }
 );
 
 UserSchema.plugin(uniqueValidator, {
-	message: '{VALUE} böyle bir kullanıcı zaten var!'
+  message: '{VALUE} böyle bir kullanıcı zaten var!'
 });
 
 // eslint-disable-next-line func-names
 UserSchema.pre('save', function(next) {
-	if (this.isModified('password')) {
-		this.password = this._hashPassword(this.password);
-	}
-	next();
+  if (this.isModified('password')) {
+    this.password = this._hashPassword(this.password);
+  }
+  next();
 });
 
 UserSchema.methods = {
-	_hashPassword(password) {
-		return hashSync(password);
-	},
-	authenticateUser(password) {
-		return compareSync(password, this.password);
-	},
-	createToken() {
-		return jwt.sign(
-			{
-				ID: this._id
-			},
-			constants.JWT_SECRET
-		);
-	},
-	toAuthJSON() {
-		return {
-			token: this.createToken()
-		};
-	},
-	toJSON() {
-		return {
-			companyID: this.companyID,
-			ID: this._id,
-			email: this.email
-		};
-	}
+  _hashPassword(password) {
+    return hashSync(password);
+  },
+  authenticateUser(password) {
+    return compareSync(password, this.password);
+  },
+  createToken() {
+    return jwt.sign(
+      {
+        ID: this._id
+      },
+      constants.JWT_SECRET
+    );
+  },
+  toAuthJSON() {
+    return {
+      token: this.createToken()
+    };
+  },
+  toJSON() {
+    return {
+      companyID: this.companyID,
+      ID: this._id,
+      email: this.email
+    };
+  }
 };
 
 module.exports = mongoose.model('User', UserSchema);
--- src/routes/public/auth.route.js
+++ src/routes/public/auth.route.js
@@ -4,11 +4,11 @@ const userController = require('./../../controllers/user.controller');
 router.post('/signup', userController.registerUser);
 
 router.post('/signin', (req, res) => {
-	res.status(200).json('signin');
+  res.status(200).json('signin');
 });
 
 router.post('/forget-password', (req, res) => {
-	res.status(200).json('forget-password');
+  res.status(200).json('forget-password');
 });
 
 module.exports = router;
