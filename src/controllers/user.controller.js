ce91320e3de782a3830dc7e8a674caf94bec7df1
Erdi Kose <me@erdkse.com>
2020-02-04 22:26:54 +0300

    add sourceType module

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,19 +3,19 @@ const userModel = require('../models/user.model');
 const userController = {};
 
 userController.registerUser = (req, res) => {
-  const userData = req.body;
-  return userModel
-    .findOne({ email: userData.email })
-    .then(data => {
-      if (data) {
-        return res.status(500).json({ message: 'Kullanıcı zaten var' });
-      }
-      // eslint-disable-next-line promise/no-return-wrap
-      return Promise.resolve(data);
-    })
-    .then(() => userModel.create({ email: userData.email, password: userData.password }))
-    .then(() => res.status(200).json({ message: 'Kullanıcı oluşturuldu' }))
-    .catch(error => res.status(500).json(error));
+	const userData = req.body;
+	return userModel
+		.findOne({ email: userData.email })
+		.then(data => {
+			if (data) {
+				return res.status(500).json({ message: 'Kullanıcı zaten var' });
+			}
+			// eslint-disable-next-line promise/no-return-wrap
+			return Promise.resolve(data);
+		})
+		.then(() => userModel.create({ email: userData.email, password: userData.password }))
+		.then(() => res.status(200).json({ message: 'Kullanıcı oluşturuldu' }))
+		.catch(error => res.status(500).json(error));
 };
 
 module.exports = userController;
--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -3,15 +3,15 @@ const Joi = require('joi');
 const passwordRegex = /(?=.*\d)(?=.*[a-z]).{6,}/;
 
 module.exports = {
-  passwordRegex,
-  signup: {
-    email: Joi.string()
-      .email()
-      .required(),
-    password: Joi.string()
-      .regex(passwordRegex)
-      .required(),
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required()
-  }
+	passwordRegex,
+	signup: {
+		email: Joi.string()
+			.email()
+			.required(),
+		password: Joi.string()
+			.regex(passwordRegex)
+			.required(),
+		firstName: Joi.string().required(),
+		lastName: Joi.string().required()
+	}
 };
